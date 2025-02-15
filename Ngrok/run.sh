292e5b7d0b3d8a09ded1988bdbfc98031f0cb2e0
0-Moi-0 <m.saby0595@gmail.com>
2025-02-15 11:56:33 -0600

    Update run.sh

--- Ngrok/run.sh
+++ Ngrok/run.sh
@@ -1,5 +1,18 @@
-#!/usr/bin/with-contenv bashio
+#!/bin/sh
 
-echo "Hello world!"
+CONFIG_PATH=/data/options.json
 
-python3 -m http.server 8000
+# Leer opciones del complemento
+authtoken=$(jq --raw-output '.authtoken' $CONFIG_PATH)
+domain=$(jq --raw-output '.domain' $CONFIG_PATH)
+
+# Instalar Ngrok si no está presente
+if [ ! -f "/usr/local/bin/ngrok" ]; then
+    curl -s https://bin.equinox.io/c/bNyj1mQVY4c/ngrok-stable-linux-arm.zip -o ngrok.zip
+    unzip ngrok.zip -d /usr/local/bin/
+    rm ngrok.zip
+fi
+
+# Autenticar y lanzar el túnel
+/usr/local/bin/ngrok authtoken "$authtoken"
+/usr/local/bin/ngrok http 8123 --hostname="$domain"
